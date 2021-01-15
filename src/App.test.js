243d2479c79f2718d665a33207439e9d6a8a163f
albertly <albertly@yandex.ru>
2021-01-15 12:16:56 +0200

    Test callback

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,40 @@
 import { render, screen } from '@testing-library/react';
+import Chalk from 'chalk';
+
 import App from './App';
 
-test('renders learn react link', () => {
+test.skip('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+describe("Async in JavaScript", () => {
+
+  const message = function(done) {  
+    console.log(Chalk.red("This is a message."));
+    if (done) {
+      done();
+    }
+  }
+
+  const f = function(cb) {
+    cb();
+  }
+
+  it("should run function", ()=>{
+    message();
+  })
+
+  it("should run callback", ()=>{
+   f(message);
+   console.log("After callback");
+  })
+
+  it.only("should run async callback", (done) => {
+    console.log(Chalk.blue("Before async"));
+ 
+    setTimeout(() => message(done), 0);
+    console.log(Chalk.blue("After async"));
+  })
+})
